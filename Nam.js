8b55c985af9224761cbc175489fbacf5beaecf11
Tan Tai <ntantai123321@gmail.com>
2023-11-11 10:18:49 +0700

    Them

--- Nam.js
+++ Nam.js
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function Nam() {
   return (
     <View style={styles.container}>
-      Nam
+        <Text>abc</Text>
+        <Text>ift</Text>
     </View>
   );
 }
